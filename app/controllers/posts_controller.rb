b320c830cf194f5bd27c7b41d16a68390e273001
Jasmeet Singh <jascee.gtbit@gmail.com>
2016-01-14 22:37:31 +0530

    Added Posts Credibility

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,43 @@
+class PostsController < ApplicationController
+	before_action :find_post, only: [:edit,:show,:update,:destroy]
+	def index
+	end
+	def new
+		@post = Post.new
+	end
+	def create
+		@post = Post.new post_params
+
+		if @post.save
+			redirect_to @post, notice: "Hellz Yeah, Jasmeet! Your Artice was successfully saved!"
+		else
+			render'new' , notice: "ohh no, Jasmeet! I  was unable to save your post"
+
+		end
+	end
+	def show
+	end
+	def edit
+	end
+	def update
+		if @post.update post_params
+			redirect_to @post, notice: "Hurray! Your Article has been saved"
+		else
+			render 'edit'
+		end
+
+	end
+	def destroy
+		@post.destroy
+		redirect_to posts_path
+	end
+private
+
+def post_params
+	params.require(:post).permit(:title, :content)
+end
+def find_post
+	@post = Post.find(params[:id])
+	
+end
+end
