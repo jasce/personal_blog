b0b8d4336f6779cfdbb67c7da2a3aba4c55395bd
Jasmeet Singh <jascee.gtbit@gmail.com>
2016-01-16 18:03:05 +0530

    Dynamic Post generation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
 	before_action :find_post, only: [:edit,:show,:update,:destroy]
 	def index
+		@posts = Post.all.order("created_at desc")
 	end
 	def new
 		@post = Post.new
