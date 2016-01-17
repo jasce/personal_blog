bad115249881aa09ab6ef88176a596b6076285d5
Jasmeet Singh <jascee.gtbit@gmail.com>
2016-01-17 13:03:55 +0530

    Added devise and authentication

--- Gemfile
+++ Gemfile
@@ -27,6 +27,7 @@ gem 'redcarpet', '~> 3.2.2'
 gem 'friendly_id','~> 5.1.0'
 gem 'will_paginate','~> 3.0.7'
 gem 'mail_form','~> 1.5.1'
+gem 'devise', '~>3.4.1'
 gem 'wdm', '~> 0.1.0' if Gem.win_platform?
 
 
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,6 @@
 class ProjectsController < ApplicationController
 	before_action :find_project, only: [:edit,:show,:update,:destroy]
-
+	before_action :authenticate_user!,except: [:index,:show]
 	def index
 		@projects =Project.all.order('created_at desc')
 	end
