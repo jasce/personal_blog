b24743be2b5f548b91d81c8762c2310670382f28
Jasmeet Singh <jascee.gtbit@gmail.com>
2016-01-17 11:48:00 +0530

    Issue sorted

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
  resources :posts
  resources :projects
- resources :contacts, only: [:new ,:create]
+
+  resources :contacts, only: [:new ,:create]
  get 'welcome/index'
  root 'welcome#index'
 end
