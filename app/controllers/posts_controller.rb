d46326970dffdcf7587f734a6f0122eb35ab63d1
Jasmeet Singh <jascee.gtbit@gmail.com>
2016-01-17 09:24:12 +0530

    Added projects functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
 	before_action :find_post, only: [:edit,:show,:update,:destroy]
 	def index
-		@posts = Post.all.order("created_at desc").paginate(page: params[:page],per_page: 2)
+		@posts = Post.all.order("created_at desc").paginate(page: params[:page],per_page: 8)
 	end
 	def new
 		@post = Post.new
---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,30 @@
+class ProjectsController < ApplicationController
+	before_action :find_project, only: [:show ,:edit, :update, :destroy]
+
+	def index
+		@projects =Project.all.order('created_at desc')
+	end
+	def create
+		@project = Project.new project_params
+		if @project.save
+			redirect_to @project, notice: "Nice Jasmeet! That Project was successfully saved"
+		else
+			render 'new'
+		end
+	end
+	def show
+		
+	end
+
+	def new
+		@project = Project.new
+	end
+	private
+	def project_params
+		params.require(:project).permit(:title, :description, :link)
+		
+	end
+	def find_project
+		@project = Project.find(params[:id])
+	end
+end
