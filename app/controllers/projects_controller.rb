1d355315d9a9ebabb7b02f0745e8c3c48e5a19ba
Jasmeet Singh <jascee.gtbit@gmail.com>
2016-01-17 10:06:32 +0530

    Add edit & delete projects abilities

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,5 @@
 class ProjectsController < ApplicationController
-	before_action :find_project, only: [:show ,:edit, :update, :destroy]
+	before_action :find_project, only: [:edit,:show,:update,:destroy]
 
 	def index
 		@projects =Project.all.order('created_at desc')
@@ -13,7 +13,19 @@ class ProjectsController < ApplicationController
 		end
 	end
 	def show
-		
+	end
+	def edit		
+	end
+	def update
+		if @project.update project_params
+			redirect_to @project, notice: "Nice Jasmeet! That Project is successfully updated"
+		else
+			render 'edit'
+		end
+	end
+	def destroy	
+		@project.destroy
+		redirect_to projects_path
 	end
 
 	def new
